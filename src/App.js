d03786282828951b88cf725321e91dd371be6f48
rodrigosgomide <rodrigogomide.def@gmail.com>
2022-08-24 23:34:57 -0300

    R5

--- src/App.js
+++ src/App.js
@@ -6,12 +6,87 @@ class App extends React.Component {
   state = {
     cardName: '',
     cardDescription: '',
-    cardAttr1: '',
-    cardAttr2: '',
-    cardAttr3: '',
+    cardAttr1: '0',
+    cardAttr2: '0',
+    cardAttr3: '0',
     cardImage: '',
-    cardRare: '',
+    cardRare: 'normal',
     cardTrunfo: false,
+    isSaveButtonDisabled: true,
+  };
+
+  realState = (key, value) => {
+    let {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardRare,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+    if (key === 'cardName') {
+      cardName = value;
+    }
+    if (key === 'cardDescription') {
+      cardDescription = value;
+    }
+    if (key === 'cardImage') {
+      cardImage = value;
+    }
+    if (key === 'cardRare') {
+      cardRare = value;
+    }
+    if (key === 'cardAttr1') {
+      cardAttr1 = value;
+    }
+    if (key === 'cardAttr2') {
+      cardAttr2 = value;
+    }
+    if (key === 'cardAttr3') {
+      cardAttr3 = value;
+    }
+
+    return {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardRare,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3 };
+  };
+
+  handlerButton = (obj) => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardRare,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = obj;
+
+    const MAX_ALLOWED_POINTS = 210;
+    const MIN_ALLOWED_ATT_POINTS = 0;
+    const MAX_ALLOWED_ATT_POINTS = 90;
+    const cardAttr1N = parseInt(cardAttr1, 10);
+    const cardAttr2N = parseInt(cardAttr2, 10);
+    const cardAttr3N = parseInt(cardAttr3, 10);
+    const total = cardAttr1N + cardAttr2N + cardAttr3N;
+
+    return !(cardName !== ''
+      && cardDescription !== ''
+      && cardImage !== ''
+      && cardRare !== ''
+      && cardAttr1N >= MIN_ALLOWED_ATT_POINTS
+      && cardAttr1N <= MAX_ALLOWED_ATT_POINTS
+      && cardAttr2N >= MIN_ALLOWED_ATT_POINTS
+      && cardAttr2N <= MAX_ALLOWED_ATT_POINTS
+      && cardAttr3N >= MIN_ALLOWED_ATT_POINTS
+      && cardAttr3N <= MAX_ALLOWED_ATT_POINTS
+      && total <= MAX_ALLOWED_POINTS);
   };
 
   handlerChange = (event) => {
@@ -21,6 +96,13 @@ class App extends React.Component {
 
     this.setState({
       [name]: value,
+    });
+
+    const x = this.realState(name, value);
+    const xxx = this.handlerButton(x);
+
+    this.setState({
+      isSaveButtonDisabled: xxx,
     });
   };
 
@@ -34,6 +116,7 @@ class App extends React.Component {
       cardImage,
       cardRare,
       cardTrunfo,
+      isSaveButtonDisabled,
     } = this.state;
     return (
       <div>
@@ -48,6 +131,7 @@ class App extends React.Component {
           cardImage={ cardImage }
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
         />
         <Card
           cardName={ cardName }
